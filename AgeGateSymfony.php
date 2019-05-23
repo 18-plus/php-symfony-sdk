5727e4ca10b1d462cb1e96c5930f81bc01ec4022
Aleksandr Stolbov <as.stolbov@gmail.com>
2019-05-23 11:58:12 +0400

    symfony 3 support

--- AgeGateSymfony.php
+++ AgeGateSymfony.php
@@ -10,13 +10,26 @@ class AgeGateSymfony extends Bundle
 {
     public function boot()
     {
+        if (php_sapi_name() === 'cli') {
+            return;
+        }
+        
         $baseUrl = $this->container->get('router')->getContext()->getBaseUrl();
         if (!$baseUrl) {            
             $baseUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
         }
         
+        $title = getenv('AGEGATE_TITLE');
+        if (!$title) {            
+            try {            
+                $title = $this->container->getParameter('AGEGATE_TITLE');
+            } catch (\Symfony\Component\DependencyInjection\Exception\InvalidArgumentException $e) {
+                $title = '';
+            }
+        }
+        
         $gate = new AgeGate($baseUrl);
-        $gate->setTitle(getenv('AGEGATE_TITLE'));
+        $gate->setTitle($title);
         $gate->run();
     }
 }
--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # AgeGateSymfony
 ## Install
 
+### For Symfony 4
 Add to file config/bundles.php
 ```php
-EighteenPlus\AgeGateSymfony\AgeGateSymfony::class => ['all' => true],
+return [
+    // ...
+    EighteenPlus\AgeGateSymfony\AgeGateSymfony::class => ['all' => true],
+]
 ```
 
+### For Symfony 3
+Add to file app/AppKernel.php
+```php
+$bundles = [
+    // ...
+    new EighteenPlus\AgeGateSymfony\AgeGateSymfony(),
+]
+```
