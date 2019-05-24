298f172635cfc14a4072af48b3259c288bde69d2
Aleksandr Stolbov <as.stolbov@gmail.com>
2019-05-24 15:06:44 +0400

    set start from

--- AgeGateSymfony.php
+++ AgeGateSymfony.php
@@ -19,17 +19,37 @@ class AgeGateSymfony extends Bundle
             $baseUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
         }
         
-        $title = getenv('AGEGATE_TITLE');
+        $title = getenv('agegate_title');
         if (!$title) {            
             try {            
-                $title = $this->container->getParameter('AGEGATE_TITLE');
+                $title = $this->container->getParameter('agegate_title');
             } catch (\Symfony\Component\DependencyInjection\Exception\InvalidArgumentException $e) {
                 $title = '';
             }
         }
         
+        $testIp = getenv('agegate_test_ip');
+        if (!$testIp) {            
+            try {            
+                $testIp = $this->container->getParameter('agegate_test_ip');
+            } catch (\Symfony\Component\DependencyInjection\Exception\InvalidArgumentException $e) {
+                $testIp = '';
+            }
+        }
+        
+        $startFrom = getenv('agegate_start_from');
+        if (!$startFrom) {            
+            try {            
+                $startFrom = $this->container->getParameter('agegate_start_from');
+            } catch (\Symfony\Component\DependencyInjection\Exception\InvalidArgumentException $e) {
+                $startFrom = '';
+            }
+        }
+        
         $gate = new AgeGate($baseUrl);
         $gate->setTitle($title);
+        $gate->setTestIp($testIp);
+        $gate->setStartFrom($startFrom);
         $gate->run();
     }
 }
